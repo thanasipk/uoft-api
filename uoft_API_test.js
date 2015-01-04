f903ad27e16e28b649de4a48e51716e9a759f138
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-01-04 16:11:04 -0500

    Checkin draft test suite file.

---
+++ uoft_API_test.js
@@ -0,0 +1,48 @@
+var uoftAPI = require('./uoft_API.js');
+
+/*************************************
+        API FUNCTION TESTS
+/*************************************
+*************************************
+getCourseCodes          --
+getCourseInfo           --
+getCourseDepartment     -- IN-PROGRESS
+getProgramCourses       --
+*************************************/
+
+/*************************************
+  Tests: getCourseDepartment
+*************************************/
+// Case: Course code has all courses in one department
+// Returns: Array of a single department name string
+uoftAPI.getCourseDepartment('csc', function(err, departments) {
+  console.log(departments);
+});
+
+// Case: Course code has courses in multiple departments
+// Returns: Array of each department names as a string
+uoftAPI.getCourseDepartment('env', function(err, departments) {
+  console.log(departments);
+});
+
+// Case: Get all departments (no course code)
+// Returns: Array of all department names as strings
+uoftAPI.getCourseDepartment('', function(err, departments) {
+  console.log(departments);
+});
+
+// Case: Invalid course code (not 3-letter)
+// Returns: Empty array (fails silently)
+uoftAPI.getCourseDepartment('phil', function(err, departments) {
+  console.log(departments);
+});
+
+/*************************************
+  Tests: getProgramCourses
+*************************************/
+/*************************************
+  Tests: getCourseInfo
+*************************************/
+/*************************************
+  Tests: getCourseCodes
+*************************************/
