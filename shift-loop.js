5055123f027153fa1122ceb963544c503a28c1a0
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-01-04 19:44:51 -0500

    Make getProgramCourses take a course code and obtain its URL. Correct test documentation. The function is now entirely asynchronus.

--- shift-loop.js
+++ shift-loop.js
@@ -23,21 +23,14 @@ function getProgramURL(course, callback) {
         var currentLink = $(this);
         // Every course link has its course code
         if (webPageRegex.test(currentLink.attr('href').toString()) &&
-            currentLink.text().toString().indexOf(
-              '[' + course.toUpperCase() + ' courses') > -1) {
-              callback(null, {
-                link: currentLink.attr('href').toString()
-              });
+            currentLink.text().toString().indexOf('[' + course.toUpperCase() + ' courses') > -1) {
+              callback(null, currentLink.attr('href').toString());
               return;
         };
       });
     };
   });
 };
-
-getProgramURL('csc', function(err, linkJSON) {
-  console.log(linkJSON.link);
-});
 
 function getProgramURLs(callback) {
   request(baseURL + courseBaseURL + courseListingsURL, function(error, response, body) {
@@ -66,90 +59,97 @@ function getProgramURLs(callback) {
 Retrieves all the courses in a program's
 URL and stores the results in a file.
 */
-function getProgramCourses(singleURL, callback) {
+function getProgramCourses(course, callback) {
 
-  request(baseURL + courseBaseURL + '/' + singleURL, function(error, response, body) {
+  getProgramURL(course, function(err, courseURL) {
 
-    // Something went wrong
-    if (!error && response.statusCode == 200) {
-      // Load the new markup from this request.
-      var $                   = cheerio.load(body);
-      var courseCodeRegex     = new RegExp(/([A-Za-z]{3})([0-9]{3})([A-Za-z]{1})([0-9]{1})/);
-      var courseTutorialRegex = new RegExp(/(T|P)[0-9]{4}/);
-      var courseSectionRegex  = new RegExp(/(L)([0-9]{4})/);
-      var courseTermRegex     = new RegExp(/(F|S|Y)/i);
-      var courseWaitListRegex = new RegExp(/Y|N/);
-      var coursesJSONArray    = [];
+    request(baseURL + courseBaseURL + '/' + courseURL, function(error, response, body) {
 
-      // Get the course code from the page markup
-      $('tr').each(function(foundCourse) {
-        // Lock iteration legnth for development and testing purposes
-        if(foundCourse < 200) {
-          var courseSect = [];
+      // Something went wrong
+      if (!error && response.statusCode == 200) {
+        // Load the new markup from this request.
+        var $                   = cheerio.load(body);
+        var courseCodeRegex     = new RegExp(/([A-Za-z]{3})([0-9]{3})([A-Za-z]{1})([0-9]{1})/);
+        var courseTutorialRegex = new RegExp(/(T|P)[0-9]{4}/);
+        var courseSectionRegex  = new RegExp(/(L)([0-9]{4})/);
+        var courseTermRegex     = new RegExp(/(F|S|Y)/);
+        var courseWaitListRegex = new RegExp(/Y|N/);
+        var coursesJSONArray    = [];
 
-          var currentRow = $(this);
-          var section = currentRow.children().last();
-          var count = 0;
+        // Get the course code from the page markup
+        $('tr').each(function(foundCourse) {
+          // Lock iteration legnth for development and testing purposes
+          if(foundCourse < 1000) {
+            var courseSect = [];
 
-          /* At most 10 cells in the row, check until the end to be sure */
-          while(!courseSectionRegex.test(section.text().toString()) && count < 9) {
-            section = section.prev();
-            count++;
-          };
+            var currentRow = $(this);
+            var section = currentRow.children().last();
+            var count = 0;
 
-          /* Once the lecture section is found, the other data is relative to its position */
-          courseWait = section.next();
-          courseName = section.prev();
-          courseTerm = section.prev().prev();
-          courseCode = section.prev().prev().prev();
-          courseProfessor = section.next().next().next().next();
-
-          console.log("----------------------------------------");
-          // KEEP HERE FOR TESTING PURPOSES
-          // console.log('section found as: ' + section.text().toString());
-          // console.log('courseName found as: ' + courseName.text().toString());
-          // console.log('courseTerm found as: ' + courseTerm.text().toString());
-          // console.log('courseCode found as: ' + courseCode.text().toString());
-        };
-
-        // Push valid course data (lecture data only) to the course JSON array
-        /*
-          Reminder: this will only push lecture section data rows because
-          the AND forces all fields to be filled in, which is not the case with
-          tutorial rows.
-        */
-
-        if(courseSectionRegex.test(section.text().toString()) &&
-           courseTermRegex.test(courseTerm.text().toString())    &&
-           courseCodeRegex.test(courseCode.text().toString())) {
-
-            // Should loop to get the sections repeatedly
-            var courseSections = [];
-            var courseProfessors = [];
-
-            // May be multiple profs for one course section, append them
-            if (courseProfessor.text().toString().indexOf('/') > -1) {
-              courseProfessors = courseProfessor.text().toString().split('/');
-            } else {
-              courseProfessors.push(courseProfessor.text().toString());
-            }
-
-            courseSections.push(section.text().toString());
-
-            course = {
-              courseName: courseName.text().toString(),
-              courseCode: courseCode.text().toString(),
-              CourseTerm: courseTerm.text().toString(),
-              CourseSect: courseSections,
-              courseWait: courseWait.text().toString(),
-              courseProf: courseProfessors
+            /* At most 10 cells in the row, check until the end to be sure */
+            while(!courseSectionRegex.test(section.text().toString()) && count < 9) {
+              section = section.prev();
+              count++;
             };
 
-            coursesJSONArray.push(course);
-            console.log(JSON.stringify(course));
-        };
-      });
-    };
-    callback(null, coursesJSONArray);
+            /* Once the lecture section is found, the other data is relative to its position */
+            courseWait = section.next();
+            courseName = section.prev();
+            courseTerm = section.prev().prev();
+            courseCode = section.prev().prev().prev();
+            courseProfessor = section.next().next().next().next();
+
+            console.log("----------------------------------------");
+            // KEEP HERE FOR TESTING PURPOSES
+            // console.log('section found as: ' + section.text().toString());
+            // console.log('courseName found as: ' + courseName.text().toString());
+            // console.log('courseTerm found as: ' + courseTerm.text().toString());
+            // console.log('courseCode found as: ' + courseCode.text().toString());
+          };
+
+          // Push valid course data (lecture data only) to the course JSON array
+          /*
+            Reminder: this will only push lecture section data rows because
+            the AND forces all fields to be filled in, which is not the case with
+            tutorial rows.
+          */
+
+          if(courseSectionRegex.test(section.text().toString()) &&
+             courseTermRegex.test(courseTerm.text().toString())    &&
+             courseCodeRegex.test(courseCode.text().toString())) {
+
+              // Should loop to get the sections repeatedly
+              var courseSections = [];
+              var courseProfessors = [];
+
+              // May be multiple profs for one course section, append them
+              if (courseProfessor.text().toString().indexOf('/') > -1) {
+                courseProfessors = courseProfessor.text().toString().split('/');
+              } else {
+                courseProfessors.push(courseProfessor.text().toString());
+              }
+
+              courseSections.push(section.text().toString());
+
+              course = {
+                courseName: courseName.text().toString(),
+                courseCode: courseCode.text().toString(),
+                courseTerm: courseTerm.text().toString(),
+                courseWait: courseWait.text().toString(),
+                courseSect: courseSections,
+                courseProf: courseProfessors
+              };
+
+              coursesJSONArray.push(course);
+              console.log(JSON.stringify(course));
+          };
+        });
+      };
+      callback(null, coursesJSONArray);
+    });
   });
 };
+
+getProgramCourses('csc', function(err, courses) {
+  console.log(courses);
+});
