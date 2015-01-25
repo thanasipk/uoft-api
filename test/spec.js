3c07c47162c48787e38e78f19cc77a699ad713c7
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-01-25 18:48:57 -0500

    Move tests into spec.js. Add placeholder tests.

---
+++ test/spec.js
@@ -0,0 +1,28 @@
+// var index = require('../index.js');
+
+exports.testGetAllCourseDepartments = {
+  testGetAllDepartments : function(test){
+    test.done();
+  }
+};
+
+exports.testGetCourseDepartment = {
+  testSingleAssociatedDepartment : function(test) {
+    test.done();
+  },
+  testManyAssociatedDepartment : function(test) {
+    test.done();
+  },
+  testBadCourseCode : function(test) {
+    test.done();
+  }
+};
+
+exports.testGetProgramCourses = {
+  testIncludeCancelledCourses : function(test){
+    test.done();
+  },
+  testNotIncludeCancelledCourses : function(test){
+    test.done();
+  }
+};
